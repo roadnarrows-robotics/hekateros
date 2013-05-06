bf87ba7c436e5364e14bea34d3121abd95c4b63e
Daniel Packard <daniel@roadnarrows.com>
2013-05-06 11:33:29 -0600

    snap

--- hekateros_control/src/hekateros_control_client.cxx
+++ hekateros_control/src/hekateros_control_client.cxx
@@ -13,10 +13,11 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "hek_control_client");
   ros::NodeHandle n;
 
-  ros::ServiceClient client = n.serviceClient<hekateros_control::QueryVersion>("hek_version");
+  ros::ServiceClient versionClient = 
+       n.serviceClient<hekateros_control::QueryVersion>("hek_version");
 
   hekateros_control::QueryVersion srv;
-  if(client.call(srv))
+  if(versionClient.call(srv))
   {
     ROS_INFO("Hekateros Version: %d.%d.%d-%s", (int)srv.response.v.maj, (int)srv.response.v.min, (int)srv.response.v.rev, srv.response.v.configuration.c_str());
   }
