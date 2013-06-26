af4ff0059a74b6b5fec2b704444980d54cd83e7e
Daniel Packard <daniel@roadnarrows.com>
2013-06-26 17:22:13 -0600

    setting up action server

--- hekateros_control/src/hekateros_control.cpp
+++ hekateros_control/src/hekateros_control.cpp
@@ -1,13 +1,34 @@
 //
 //  TODO (dhp) - set header
 //
-
 #include "ros/ros.h"
+#include "actionlib/server/simple_action_server.h"
 
 #include "rnr/rnrconfig.h"
+#include "rnr/log.h"
 
+#include "Hekateros/hekateros.h"
+#include "Hekateros/hekXmlCfg.h"
+#include "Hekateros/hekRobot.h"
+
+#include "hekateros_control/CalibrateAction.h"
 #include "hekateros_control/QueryVersion.h"
 #include "hekateros_control/Version.h"
+
+using namespace ::hekateros;
+HekRobot *pRobot = NULL;
+
+typedef actionlib::SimpleActionServer<hekateros_control::CalibrateAction> 
+                                                                   CalibrateAS;
+
+void CalibrateCB(const hekateros_control::CalibrateGoalConstPtr& goal, 
+                  CalibrateAS* as)
+{
+  // do awesome calibration stuff here
+  ROS_INFO("Calibrating Hekateros... It's a Hek of a good time!");
+  // DHP - TODO calibration stuff here
+  as->setSucceeded();
+}
 
 bool hek_version(hekateros_control::QueryVersion::Request &req,
                  hekateros_control::QueryVersion::Response &rsp)
@@ -25,12 +46,19 @@ bool hek_version(hekateros_control::QueryVersion::Request &req,
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "hek_control_server");
-  ros::NodeHandle n;
+
+  ros::NodeHandle nh_;
 
   ros::ServiceServer versionService = 
-       n.advertiseService("hek_version", hek_version);
+       nh_.advertiseService("hek_version", hek_version);
 
-  ROS_INFO("Hekateros Server up and running - Get your arm on!");
+  // action servers
+  CalibrateAS calibAS(nh_, "calibrate", 
+                       boost::bind(&CalibrateCB, _1, &calibAS), 
+                       false);
+  calibAS.start();
+
+  ROS_INFO("Hekateros Control Server - ready for action!");
   ros::spin();
 
   return 0;
