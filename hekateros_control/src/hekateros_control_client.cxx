53813f0f7214f4a894ab7ae978e682ae81d1addb
Daniel Packard <daniel@roadnarrows.com>
2013-06-26 17:22:13 -0600

    setting up action server
    Former-commit-id: 92d3f32b13c5bb36f7276db8222ec3016f6839e4

--- hekateros_control/src/hekateros_control_client.cxx
+++ hekateros_control/src/hekateros_control_client.cxx
@@ -5,14 +5,29 @@
 #include <string>
 #include "ros/ros.h"
 
-
 #include "hekateros_control/QueryVersion.h"
+#include "hekateros_control/CalibrateAction.h"
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<hekateros_control::CalibrateAction> Client;
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "hek_control_client");
+  ROS_INFO("here1");
   ros::NodeHandle n;
 
+  Client client("calibrate", true); // true -> don't need ros::spin()
+  ROS_INFO("here1");
+  client.waitForServer();
+  ROS_INFO("here1");
+  //hekateros_control::CalibrateGoal goal;
+  //client.sendGoal(goal);
+  //client.waitForResult(ros::Duration(5.0));
+  //if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    //printf("Yay! the robot is calibrated!");
+  //printf("Current State: %s\n", client.getState().toString().c_str());
+  
   ros::ServiceClient versionClient = 
        n.serviceClient<hekateros_control::QueryVersion>("hek_version");
 
