5672a31f9a41ffaad90199625f0f9d435a244bbe
Daniel Packard <daniel@roadnarrows.com>
2013-06-27 15:17:11 -0600

    new hek control stuff :D

--- hekateros_control/src/hekateros_control.cpp
+++ hekateros_control/src/hekateros_control.cpp
@@ -1,58 +1,89 @@
 //
 //  TODO (dhp) - set header
 //
+#include <string>
+#include "rnr/rnrconfig.h"
+#include "rnr/log.h"
+
 #include "ros/ros.h"
 #include "actionlib/server/simple_action_server.h"
 
-#include "rnr/rnrconfig.h"
-#include "rnr/log.h"
+#include "control_msgs/FollowJointTrajectoryFeedback.h"
+#include "hekateros_control/CalibrateAction.h"
 
 #include "Hekateros/hekateros.h"
 #include "Hekateros/hekXmlCfg.h"
 #include "Hekateros/hekRobot.h"
 
-#include "hekateros_control/CalibrateAction.h"
-#include "hekateros_control/QueryVersion.h"
-#include "hekateros_control/Version.h"
 
 using namespace ::hekateros;
 HekRobot *pRobot = NULL;
 
+/*!
+ *  \brief Calibrate Action Server
+ */
 typedef actionlib::SimpleActionServer<hekateros_control::CalibrateAction> 
                                                                    CalibrateAS;
 
-void CalibrateCB(const hekateros_control::CalibrateGoalConstPtr& goal, 
+/*!
+ * \brief Calibrate Action Callback
+ */
+void CalibrateCB(const hekateros_control::CalibrateGoalConstPtr& req, 
                   CalibrateAS* as)
 {
-  // do awesome calibration stuff here
-  ROS_INFO("Calibrating Hekateros... It's a Hek of a good time!");
-  // DHP - TODO calibration stuff here
+  ROS_INFO("Calibrating Hekateros... please stand by.");
+
+  // do awesome stuff here
+
   as->setSucceeded();
-}
-
-bool hek_version(hekateros_control::QueryVersion::Request &req,
-                 hekateros_control::QueryVersion::Response &rsp)
-{
-  ROS_INFO("Serving 'QueryVersion' request (DUMMY)");
-  rsp.v.maj=0;
-  rsp.v.min=0;
-  rsp.v.rev=0;
-
-  rsp.v.configuration="Deluxe";
-
-  return true;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "hek_control_server");
-
   ros::NodeHandle nh_;
 
-  ros::ServiceServer versionService = 
-       nh_.advertiseService("hek_version", hek_version);
+  pRobot = new HekRobot;
 
-  // action servers
+  // hek bringup: load and parse hekateros.conf
+  // DHP TODO - make config_fn configurable as input param
+  string config_fn = "/etc/hekateros.conf"; 
+  HekXmlCfg xml;
+  if( xml.loadFile(config_fn) < 0 )
+  {
+    ROS_ERROR("Error: Loading XML file '%s' failed.\n", config_fn.c_str());
+    return -1; // DHP TODO - find suitable error code
+  }
+  else if( xml.setHekDescFromDOM(*pRobot->getHekDesc()) < 0 )
+  {
+    ROS_ERROR("Error: Setting robot description failed.\n");
+    return -1; // DHP TODO - find suitable error code
+  }
+  else if( pRobot->getHekDesc()->markAsDescribed() < 0 )
+  {
+    ROS_ERROR("Error: Failed to finalize descriptions.\n");
+    return -1; // DHP TODO - find suitable error code
+  }
+  else
+  {
+    ROS_INFO("Robot description complete for %s.\n",
+       pRobot->getHekDesc()->getFullProdBrief().c_str());
+  }
+
+  //hek bringup: connect to robot
+  // DHP TODO - make dev_name configurable as input param
+  string dev_name = "/dev/ttyUSB0";
+  if( pRobot->connect(dev_name) < 0 )
+  {
+    ROS_ERROR("Error: Failed to connnect to '%s'.\n", dev_name.c_str());
+    return -1; // DHP TODO - find suitable error code
+  }
+  else
+  {
+    ROS_INFO("Robot connected.\n");
+  }
+
+  // setup calibrate action server
   CalibrateAS calibAS(nh_, "calibrate", 
                        boost::bind(&CalibrateCB, _1, &calibAS), 
                        false);
