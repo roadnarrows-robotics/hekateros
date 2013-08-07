1b736c8f02e4820e479192e5f728b537a6f1cc10
Daniel Packard <daniel@roadnarrows.com>
2013-08-07 10:32:46 -0600

    added release_estop service

--- hekateros_control/src/hekateros_control.cpp
+++ hekateros_control/src/hekateros_control.cpp
@@ -102,6 +102,9 @@ ROS_WARN("dhp - here 1");
   ros::ServiceServer release_srv        = n.advertiseService("release", 
                                                              Release);
  
+  ros::ServiceServer release_estop_srv    = n.advertiseService("release_estop", 
+                                                             ReleaseEStop);
+ 
   ros::ServiceServer set_robot_mode_srv = n.advertiseService("set_robot_mode", 
                                                              SetRobotMode);
  
