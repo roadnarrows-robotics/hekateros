b1fe06a88480648910fcb7d02ff6232fdbde5f3f
Robin Knight <rknight@sharetheroad.(none)>
2014-09-03 17:49:53 -0600

    fixn

--- hekateros_control/src/hekateros_control.h
+++ hekateros_control/src/hekateros_control.h
@@ -74,7 +74,6 @@
 #include "ros/ros.h"
 #include "actionlib/server/simple_action_server.h"
 #include "control_msgs/FollowJointTrajectoryAction.h"
-#include "control_msgs/FollowJointTrajectoryAction.h"
 
 //
 // ROS generated core, industrial, and hekateros messages.
